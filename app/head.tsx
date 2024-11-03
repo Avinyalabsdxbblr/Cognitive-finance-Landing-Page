e266071e065722339d901e04b3ec31a807a8de78
Basit46 <hassanbasitope@gmail.com>
2024-11-03 22:37:11 +0100

    styles: update

---
+++ app/head.tsx
@@ -0,0 +1,41 @@
+export default function Head() {
+  return (
+    <>
+      <title>Cognitive Finance</title>
+      <meta
+        name="description"
+        content="Supercharging Your On-Chain Experience"
+      />
+
+      {/* Facebook Meta Tags */}
+      <meta property="og:url" content="https://cognitivefinance.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Cognitive Finance" />
+      <meta
+        property="og:description"
+        content="Supercharging Your On-Chain Experience"
+      />
+      <meta
+        property="og:image"
+        content="https://opengraph.b-cdn.net/production/images/9fc9ef44-05b9-4184-afb5-3a46b7029c15.png?token=Wm0U9igdj488baVcNiEWp8vmCv8JyLIyihyegJWuVK8&height=693&width=1200&expires=33266669521"
+      />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="cognitivefinance.vercel.app" />
+      <meta
+        property="twitter:url"
+        content="https://cognitivefinance.vercel.app/"
+      />
+      <meta name="twitter:title" content="Cognitive Finance" />
+      <meta
+        name="twitter:description"
+        content="Supercharging Your On-Chain Experience"
+      />
+      <meta
+        name="twitter:image"
+        content="https://opengraph.b-cdn.net/production/images/9fc9ef44-05b9-4184-afb5-3a46b7029c15.png?token=Wm0U9igdj488baVcNiEWp8vmCv8JyLIyihyegJWuVK8&height=693&width=1200&expires=33266669521"
+      />
+    </>
+  );
+}
