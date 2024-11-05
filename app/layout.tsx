b6bcfde5e126d311c0dc884740ba300ef9a6b062
Basit46 <hassanbasitope@gmail.com>
2024-11-05 16:11:23 +0100

    styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,28 @@ export const metadata: Metadata = {
   icons: {
     icon: "/logo.png",
   },
+  openGraph: {
+    url: "https://cognitivefinance.vercel.app",
+    type: "website",
+    title: "Cognitive Finance",
+    description: "Supercharging Your On-Chain Experience",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/images/9fc9ef44-05b9-4184-afb5-3a46b7029c15.png?token=3D-kTH0krfjbiPiKPY-hgt1R7eQEkCqeqvNV9NuEakQ&height=693&width=1200&expires=33266819156",
+        width: 1200,
+        height: 693,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://cognitivefinance.vercel.app",
+    title: "Cognitive Finance",
+    description: "Supercharging Your On-Chain Experience",
+    images: [
+      "https://opengraph.b-cdn.net/production/images/9fc9ef44-05b9-4184-afb5-3a46b7029c15.png?token=3D-kTH0krfjbiPiKPY-hgt1R7eQEkCqeqvNV9NuEakQ&height=693&width=1200&expires=33266819156",
+    ],
+  },
 };
 
 export default function RootLayout({
