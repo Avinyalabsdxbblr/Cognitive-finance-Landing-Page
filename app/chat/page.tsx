efb261b36456d51350cbbe5a1e3de5e2fa89bef6
Basit46 <hassanbasitope@gmail.com>
2024-12-28 16:12:07 +0100

    style: Chat UI

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
+import UserMessage from "./components/UserMessage";
+import AiMessage from "./components/AiMessage";
 import Header from "./components/Header.jsx";
-import UserMessage from "./components/UserMessage.jsx";
-import AiMessage from "./components/AiMessage.jsx";
 
 const page = () => {
   return (
     <div className="relative size-full flex flex-col">
       <Header />
 
-      <div className="chat-container mt-[10px] flex-1 px-[50px] h-full overflow-y-auto pb-[30px]">
+      <div className="chat-container mt-[10px] flex-1 px-[20px] sm:px-[50px] h-full overflow-y-auto pb-[30px]">
         <div className="h-fit w-full flex flex-col gap-[20px]">
           {/* <UserMessage />
           <AiMessage />
@@ -17,13 +17,13 @@ const page = () => {
         </div>
       </div>
 
-      <div className="h-[100px] pt-[5px] px-[50px] flex justify-end gap-[15px]">
+      <div className="h-[80px] sm:h-[100px] pt-[5px] px-[20px] sm:px-[50px] flex justify-end gap-[15px]">
         <input
           type="text"
-          className="w-[71%] h-[54px] border border-[#E2E8F0] rounded-[45px] px-[20px] outline-none"
+          className="w-full sm:w-[71%] h-[45px] sm:h-[54px] border border-[#E2E8F0] rounded-[45px] px-[20px] outline-none"
           placeholder="Enter your question"
         />
-        <button className="chat-btn w-[192px] h-[54px] rounded-[45px] text-[14px] font-[600] text-white">
+        <button className="chat-btn w-[100px] sm:w-[192px] h-[45px] sm:h-[54px] rounded-[45px] text-[14px] font-[600] text-white">
           Submit
         </button>
       </div>
